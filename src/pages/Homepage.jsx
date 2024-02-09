0194e782fe19010d518dadb318d83e48dbbc4ca5
Soulbolt <soulbolt@gmail.com>
2024-02-09 15:41:17 -0700

    Add a css file to style naviation. Add AppNav, AppLayout, components. modify link on homepage to link to App. new components place holders. Add AppNav component inside AppLayout.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
       <h1>Worldwise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
