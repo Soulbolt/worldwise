0ab7dd9b2d56c6bcb219d34c72f11b487961853d
Soulbolt <soulbolt@gmail.com>
2024-02-10 10:21:22 -0700

    Add lat and lng params setting a query string using url. Display info on map component when city is clicked. City and url should reflect string query with their respective latitude and longitude. Add a change position button to a set position params to demo url state chaning for every component that has access to it.

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,6 +10,9 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
   // TEMP DATA
   const { id } = useParams();
   const currentCity = {
@@ -21,7 +24,14 @@ function City() {
 
   const { cityName, emoji, date, notes } = currentCity;
 
-  return <h1>City {id}</h1>;
+  return (
+    <>
+      <h1>City {id}</h1>
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
+  );
 
   // return (
   //   <div className={styles.city}>
