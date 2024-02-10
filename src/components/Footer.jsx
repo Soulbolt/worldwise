5afc914d8fdae58d0259a03021249ded7d0f8758
Soulbolt <soulbolt@gmail.com>
2024-02-09 23:37:58 -0700

    Fix footer position to display at the bottom. Add maps to sidebar component. Remove unnecessary import.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,11 @@ import styles from "./Footer.module.css";
 
 function Footer() {
   return (
-    <div>
-      <footer className={styles.footer}></footer>
+    <footer className={styles.footer}>
       <p className={styles.copyrite}>
         &copy; Copyright {new Date().getFullYear()} by Worldwise Inc.
       </p>
-    </div>
+    </footer>
   );
 }
 
