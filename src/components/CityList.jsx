3e402fcf4a4d33ee1e815f0b1d8cae6cd9189bbe
Soulbolt <soulbolt@gmail.com>
2024-02-10 02:00:17 -0700

    Add CityList component, Add style, Add CityList component to the App component. Add useEffect and state variables for loading and cities to fetch cities data from local server.

---
+++ src/components/CityList.jsx
@@ -0,0 +1,8 @@
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  return <ul className={styles.cityList}>LIST</ul>;
+}
+export default CityList;
