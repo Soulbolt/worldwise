cb60a8f1bd06b488982b99393e95510eadbed5e0
Soulbolt <soulbolt@gmail.com>
2024-02-10 21:10:25 -0700

    Add button to use users gelocation and add conditional to show button if geolocation is null.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,6 +10,8 @@ import {
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import styles from "./Map.module.css";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
 
 const flagemojiToPNG = (flag) => {
   var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
@@ -23,8 +25,13 @@ const flagemojiToPNG = (flag) => {
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -35,8 +42,21 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geoLocationPosition)
+        setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng]);
+    },
+    [geoLocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your poistion"}
+        </Button>
+      )}
       <MapContainer
         center={mapLat && mapLng ? [mapLat, mapLng] : mapPosition}
         zoom={6}
