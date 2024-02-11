b60fd20c499736d48691a059a3f3982f2943fe45
Soulbolt <soulbolt@gmail.com>
2024-02-10 23:22:12 -0700

    Add conditionals to show error messages on the for sidebar component when clicking on the map that returns no country code, lat & lng, and avoid missing flag if no country code. Install datepicker for each date input.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,8 @@ import { useUrlPosition } from "../hooks/useUrlPosition";
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import Message from "./Message";
+import Spinner from "./Spinner";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -35,20 +37,31 @@ function Form() {
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
   const [emoji, setEmoji] = useState("");
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(
     function () {
+      if (!lat && !lng) return;
+
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
+          setGeocodingError("");
           const res = await fetch(
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
+
+          if (!data.countryCode)
+            throw new Error(
+              "That doesn't seem to be a city. Click somewhere else."
+            );
+
           setCityName(data.city || data.locality);
           setCountry(data.countryName);
           setEmoji(convertToEmoji(data.countryCode));
         } catch (err) {
+          setGeocodingError(err.message);
         } finally {
           setIsLoadingGeocoding(false);
         }
@@ -58,8 +71,18 @@ function Form() {
     [lat, lng]
   );
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map." />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -67,7 +90,9 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        <span className={styles.flag}>{flagemojiToPNG(emoji)}</span>
+        <span className={styles.flag}>
+          {emoji ? flagemojiToPNG(emoji) : ""}
+        </span>
       </div>
 
       <div className={styles.row}>
